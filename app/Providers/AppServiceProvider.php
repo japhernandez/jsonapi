fe0e11da30fe687641f1422e00e6fc76acd1d7c8
John Piedrahita <citymanager02@gmail.com>
2020-07-16 02:23:37 -0500

    Finalizacion de la api didactica con TDD

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use App\JsonApi\JsonApiBuilder;
+use CloudCreativity\LaravelJsonApi\LaravelJsonApi;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +27,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+       LaravelJsonApi::defaultApi('v1');
     }
 }
